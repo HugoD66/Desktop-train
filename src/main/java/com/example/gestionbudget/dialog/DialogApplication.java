6cbbdbcb342135b64532290215cbb885d602bc0c
Dessauw Hugo <dessauw.hugo@gmail.com>
2024-03-02 10:37:32 +0100

    clean code

--- src/main/java/com/example/gestionbudget/dialog/DialogApplication.java
+++ src/main/java/com/example/gestionbudget/dialog/DialogApplication.java
@@ -1,14 +1,7 @@
 package com.example.gestionbudget.dialog;
 
 import com.example.gestionbudget.line.Line;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.TableView;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 
 public class DialogApplication   {
 
